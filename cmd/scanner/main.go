30da19c55c4257b0066a5f78a71914e17039f615
Stepan Basharov <stepan.basharov@rt.ru>
2025-12-08 03:00:08 +0300

    add target and banner

--- cmd/scanner/main.go
+++ cmd/scanner/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
+	"os"
 
 	"offsec-scan-go/internal/adapters/host"
 	"offsec-scan-go/internal/adapters/nmap"
@@ -10,17 +13,55 @@ import (
 	"offsec-scan-go/pkg/logger"
 )
 
+func showBanner() {
+	version := "0.01"
+	banner := fmt.Sprintf(`
+ ________  ________ ________ ________  ________  ________  ________   ________  ________     
+|\   __  \|\  _____\\  _____\\   ____\|\   ____\|\   __  \|\   ___  \|\   ____\|\   __  \    
+\ \  \|\  \ \  \__/\ \  \__/\ \  \___|\ \  \___|\ \  \|\  \ \  \\ \  \ \  \___|\ \  \|\  \   
+ \ \  \\\  \ \   __\\ \   __\\ \_____  \ \  \    \ \   __  \ \  \\ \  \ \  \  __\ \  \\\  \  
+  \ \  \\\  \ \  \_| \ \  \_| \|____|\  \ \  \____\ \  \ \  \ \  \\ \  \ \  \|\  \ \  \\\  \ 
+   \ \_______\ \__\   \ \__\    ____\_\  \ \_______\ \__\ \__\ \__\\ \__\ \_______\ \_______\
+    \|_______|\|__|    \|__|   |\_________\|_______|\|__|\|__|\|__| \|__|\|_______|\|_______|
+                               \|_________| v%s
+`, version)
+
+	fmt.Println(banner)
+}
+
 func main() {
 	var (
-		log = logger.NewZeroLogger()
+		log    = logger.NewZeroLogger()
+		target string
 	)
+
+	flag.StringVar(&target, "t", "", "Target domain to scan (shorthand)")
+	flag.StringVar(&target, "target", "", "Target domain to scan")
+	flag.Parse()
+
+	if target == "" {
+		showBanner()
+		if _, err := fmt.Fprintf(os.Stderr, "\nError: target domain is required\n\n"); err != nil {
+			os.Exit(1)
+		}
+		if _, err := fmt.Fprintf(os.Stderr, "Usage: %s -t <domain> or %s --target <domain>\n\n", os.Args[0], os.Args[0]); err != nil {
+			os.Exit(1)
+		}
+		if _, err := fmt.Fprintf(os.Stderr, "Flags:\n"); err != nil {
+			os.Exit(1)
+		}
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	showBanner()
 
 	subfinderAdapter := subfinder.NewSubFinder()
 	hostAdapter := host.NewHost()
 	nmapAdapter := nmap.NewNmap(log)
 
 	uc := usecase.NewBaseScanUseCase(
-		"iconlimits.store",
+		target,
 		&subfinderAdapter,
 		&hostAdapter,
 		&nmapAdapter,
@@ -31,5 +72,6 @@ func main() {
 			"Error executing usecase",
 			logger.Field{Key: "error", Value: err},
 		)
+		os.Exit(1)
 	}
 }
